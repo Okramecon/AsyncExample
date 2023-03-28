929345aa23c52785cdfefa2b0d17ebb62134add2
Akram Maflinov <akram.maflinov@gmail.com>
2023-03-28 19:04:29 +0600

    Add project files.

---
+++ AsyncExample/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Concurrent;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+using Share;
+
+//Console.WriteLine("Hello, World!");
+
+//var itemsService = new ItemsService();
+
+//List<Item> items = new();
+
+//// 1
+////for (int i = 0; i < 5; i++)
+////{
+////    var bunch = await itemsService.GetBunchOfItemsAsync(i, 50);
+////    items.AddRange(bunch);
+////}
+
+////2
+//List<Task<IEnumerable<Item>>> itemsTasks = new();
+//for (int i = 0; i < 5; i++)
+//{
+//    var bunchTask = itemsService.GetBunchOfItemsAsync(i, 50);
+//    itemsTasks.Add(bunchTask);
+//}
+
+////Task.WhenAll(itemsTasks);
+
+//items = itemsTasks.SelectMany(x => x.Result).ToList();
+
+////
+
+//Console.WriteLine("Start parallel calculation");
+//foreach (var item in items)
+//{
+//    item.Name += "_changed";
+//    //await Task.Delay(500);
+//}
+
+//foreach (var item in items)
+//{
+//    Console.WriteLine(item.Code + " * " + item.Name);
+//}
+
+//Parallel.ForEach(items, async x => { x.Name += "_changed"; await Task.Delay(500); });
+
+//foreach (var item in items)
+//{
+//    Console.WriteLine(item.Code + " * " + item.Name);
+//}
+
+// Thread
+ConcurrentDictionary<int, string> shareSource = new ConcurrentDictionary<int, string>();
+
+foreach (var item in Enumerable.Range(0, 10))
+{
+    shareSource.TryAdd(item, item.ToString());
+
+}
+
+
+var myThread = new Thread(ShareResourceRun);
+myThread.Name = "My thread";
+
+myThread.Start();
+
+var myThread1 = new Thread(ShareResourceRun);
+myThread1.Name = "My thread 1";
+
+myThread1.Start();
+
+
+void MyThreadRun()
+{
+    var threadCurrent = Thread.CurrentThread;
+    foreach (var item in Enumerable.Range(0, 10))
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine("Thread: " + threadCurrent.Name + "(" + threadCurrent.ManagedThreadId + ") " + "number: " + item);
+    }
+}
+void ShareResourceRun()
+{
+    var rnd = new Random();
+    var threadCurrent = Thread.CurrentThread;
+    foreach (var item in shareSource)
+    {
+        Thread.Sleep(rnd.Next(1000, 5000));
+        shareSource.Remove(item.Key, out string value);
+        Console.WriteLine("Thread: " + threadCurrent.Name + "(" + threadCurrent.ManagedThreadId + ") " + "number: " + value);
+    }
+}
---
+++ Share/ItemsService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Share
+{
+    public class ItemsService
+    {
+        private static List<Item> Items { get; } = new List<Item>();
+
+        static ItemsService()
+        {
+            var items = Enumerable.Range(0, 1000)
+                .Select(x => new Item(x, $"Item_Name_{x}"));
+
+            Items.AddRange(items);
+        }
+
+        public async Task<IEnumerable<Item>> GetBunchOfItemsAsync(int? start = 0, int? count = 50)
+        {
+            if (count > 50)
+                count = 50;
+
+            // 2
+            var randomDelay = new Random().Next(3000, 8000);
+
+            //
+
+            await Task.Delay(randomDelay);
+
+            return Items.Skip(start.Value * count.Value).Take(count.Value);
+        }
+
+    }
+
+    public class Item
+    {
+        public Item(int code, string name)
+        {
+            Code = code;
+            Name = name;
+        }
+
+        public int Code { get; set; }
+        public string Name { get; set; }
+    }
+}
